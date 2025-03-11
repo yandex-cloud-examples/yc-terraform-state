130ad90132eac1c7882fa125f6f0f8271f4005ce
romanzhukov <romanzhukov@yandex-team.ru>
2025-03-11 10:39:52 +0300

    DOCSUP-95374: Update backend TF example.

--- main.tf
+++ main.tf
@@ -1,25 +1,23 @@
 terraform {
+  # Настройки провадера
+
+  ...
+
+  # Настройки бэкенда
+
   backend "s3" {
-    endpoint                    = "storage.yandexcloud.net"
-    dynamodb_endpoint           = "https://docapi.serverless.yandexcloud.net/ru-central1/<CLOUD-ID>/<DATABASE-ID>"
-    region                      = "us-east-1"
+    endpoints = {
+      s3       = "https://storage.yandexcloud.net"
+      dynamodb = "<эндпоинт_Document_API_БД>"
+    }
+    bucket         = "<имя_бакета>"
+    region         = "<регион>"
+    key            = "<путь_к_файлу_состояния_в_бакете>/<имя_файла_состояния>.tfstate"
+    dynamodb_table = "<имя_таблицы>"
+
+    skip_region_validation      = true
     skip_credentials_validation = true
-    skip_metadata_api_check     = true
-
-    // state will be stored in given bucket and object
-    // bucket should be created before `terraform init`
-    bucket                      = "<BUCKET-ID>"
-    key                         = "terraform-example-state"
-
-    // state lock will be held in given table
-    // table should be created before `terrform init`
-    // with `LockID` column as Primary Key (`string`)
-    dynamodb_table              = "tf_lock"
-
-    // We're not writing access_key and secret_key here
-    // for security reasons (it's easy to commit in VCS)
-    //
-    // Use command line arguments for `terraform init` like:
-    //     -backend-config="secret_key=<secret_value>"
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
   }
 }
